3c23cd986ef2fe966712289ddebb9a0954757a09
HanhTr1 <153684620+HanhTr1@users.noreply.github.com>
2024-11-08 09:05:34 +0200

    Update ex 12.2.py

--- ex 12.2.py
+++ ex 12.2.py
@@ -1,16 +1,11 @@
 import requests
 import json
-from pint import UnitRegistry
 city_name=input("Enter city name: ")
-api_key= "3"
-url=f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}"
+api_key= "f540fba3af1f42b63f4f4669d3d52443"
+url=f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric"
 response = requests.get(url)
-print(json.dumps(response.json(),indent=2))
+#print(json.dumps(response.json(),indent=2))
 for data in response.json()['weather']:
-    print(data['main'])
-    print(data['description'])
-convert = UnitRegistry()
-temp = response.json()['main']['temp']
-kelvin_temp= temp * convert.kelvin
-celsius_temp = kelvin_temp.to(convert.celsius)
-print(f"Temperature: {celsius_temp:.2f}")
+    print("Weather: " + data['main'])
+    print("Description " + data['description'])
+print(f"Temperature : {response.json()['main']['temp']}\u00B0C")
